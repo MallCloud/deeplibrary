679fc585cd0f98f66d96e38fb2ac90c0603a283c
daemonSlayer <kpgupta98@gmail.com>
2016-10-14 10:19:52 +0530

    removed icheck

--- client/main.js
+++ client/main.js
@@ -5,10 +5,3 @@ import './main.html';
 
 
 
-$(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' // optional
-    });
-  });
