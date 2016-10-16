a04a302452416d38af3f60b0232723db8060a7aa
Tim <tim081489@yahoo.com>
2016-10-16 15:00:20 -0400

    add login and register function

--- client/main.js
+++ client/main.js
@@ -13,7 +13,64 @@ if (Meteor.isServer) {
   });
 }
 
+Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            email: email,
+            password: password
+        });
+        console.log("registered successfully")
+    }
+});
+Template.login.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Meteor.loginWithPassword(email, password, function(error){
+            if(error){
+                console.log(error.reason);
+            } else {
+                Router.go("/input");
+            }
+        });
+    }
+});
+/*Template.login.onRendered(function(){
+    $('.login').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 6
+            }
+        },
+        messages: {
+            email: {
+                required: "You must enter an email address.",
+                email: "You've entered an invalid email address."
+            },
+            password: {
+                required: "You must enter a password.",
+                minlength: "Your password must be at least {0} characters."
+            }
+        }
+    });
+});*/
 
+Template.input.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+        Router.go('/login');
+    }
+});
 Template.uploadedFiles.helpers({
   uploadedFiles: function () {
     return Images.find();
